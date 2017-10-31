29a61adffd0d717bdd13c70009a6aa5fa9f03b27
Vendettan <32261725+Vendettan@users.noreply.github.com>
2017-10-31 16:47:34 +0000

    Core Game Mechanic Commit
    Fundamental move, jump and camera mechanics

---
+++ Game_Mechanic/Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform player;
+
+    private float smoothing = 5.0f;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(player.position.x, player.position.y, -20);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * Time.fixedDeltaTime));        
+    }
+
+
+
+}
---
+++ Game_Mechanic/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    private float acceleration = 5.0f;
+    private float maxSpeed = 13.0f;
+    private float jumpPower = 400.0f;
+    private bool isGrounded;
+
+
+    void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();        
+      
+    }
+	
+
+	void Update ()
+    {                           
+        rigidbody.AddForce(transform.right * Input.GetAxis("Horizontal") * acceleration);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rigidbody.AddForce(transform.up * Input.GetAxis("Vertical") * jumpPower, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        isGrounded = true;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newVelocity = rigidbody.velocity;
+
+        if (newVelocity.x > maxSpeed)
+        {
+            newVelocity.x = maxSpeed;
+        }
+
+        if (newVelocity.y > maxSpeed)
+        {
+            newVelocity.y = maxSpeed;
+        }
+
+        rigidbody.velocity = newVelocity;
+    }
+}
